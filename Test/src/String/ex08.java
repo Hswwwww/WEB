94584970bf920be929385b93c6f38ea131f8da48
Hswwwww <southm123@naver.com>
2024-03-19 14:54:36 +0900

    web

--- Test/src/String/ex08.java
+++ Test/src/String/ex08.java
@@ -3,21 +3,27 @@ package String;
 import java.util.Scanner;
 
 public class ex08 {
-    public static String solution(String s){
-        String answer="NO";
-        String Us = s.toUpperCase();
-        StringBuffer sb = new StringBuffer(Us);
-        String rs = sb.reverse().toString();
-
-        if(Us.equals(rs)){
-            return answer="YES";
-        }
-        return answer;
+    public static int solution(int n , int[] arr){
+       int answer= 1;
+       int cnt = 0;
+       for(int i = 1; i<arr.length; i++) {
+    	   if(arr[i-1]>=arr[i]) {
+    		   cnt++;
+    	   }
+       }
+       
+       
+       return answer;
+       
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-        System.out.println(solution(s));
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(solution(n, arr));
     }
 }
